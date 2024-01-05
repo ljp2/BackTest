eb11e0cc97fe91b42a00a5518f2cf2c2a2e09f3f
ljp2 <ljp2@lehigh.edu>
2024-01-05 13:21:31 -0500

    working

--- main.py
+++ main.py
@@ -2,22 +2,36 @@ import sys
 import glob
 from PyQt6.QtCore import QSize, Qt
 
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, \
-    QPushButton, QComboBox
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout, \
+    QLabel, QPushButton, QComboBox, QGroupBox, QSpacerItem, QSizePolicy
 
-def getBarFiles():
-    files = [s.split('/')[-1].split('.')[0] for s in  glob.glob("/Users/ljp2/Data/*.csv")]
-    return files
+class ChooseBarFile(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        layout = QVBoxLayout()
+        
+        self.title_label = QLabel('Backtest Day')
+        group_box = QGroupBox()
+        group_layout = QVBoxLayout(group_box)
+        group_layout.addWidget(self.title_label, alignment=Qt.AlignmentFlag.AlignTop)
+        
+        self.comboBox = QComboBox()
+        files = ["None Selected"] + self.getBarFiles()
+        self.comboBox.addItems(files)
+        self.comboBox.currentIndexChanged.connect( self.index_changed )
+        self.comboBox.currentTextChanged.connect( self.text_changed )
+        
+        group_layout.addWidget(self.comboBox, alignment=Qt.AlignmentFlag.AlignTop)
+        
+        layout.addWidget(group_box)
+        spacer = QSpacerItem(0, 0, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        layout.addItem(spacer)
+        self.setLayout(layout)
 
-class BarFiles(QComboBox):
-    def __init__(self) -> None:
-        super().__init__()
-        files = [""] + getBarFiles()
-        self.addItems(files)
-        
-        self.currentIndexChanged.connect( self.index_changed )
-        self.currentTextChanged.connect( self.text_changed )
-        
+    def getBarFiles(self):
+        files = [s.split('/')[-1].split('.')[0] for s in  glob.glob("/Users/ljp2/Data/*.csv")]
+        return files
+    
     def index_changed(self, i): # i is an int
         print(i)
 
@@ -25,26 +39,32 @@ class BarFiles(QComboBox):
         print(s)
 
 class MainWindow(QMainWindow):
+
     def __init__(self):
-        super().__init__()
-        
-        self.button_is_checked = False
-        
-        self.setMinimumSize(QSize(400,300))
+        super(MainWindow, self).__init__()
+        self.setMinimumSize(QSize(800,600))
         self.setWindowTitle('BackTester')
         
-        self.files = BarFiles()
+        layout = QVBoxLayout()
         
-        self.setCentralWidget(self.files)
+        self.choose:ChooseBarFile = ChooseBarFile()
         
+        self.button = QPushButton("Show Current File Name")
+        self.button.clicked.connect(self.clicked)
         
-    def the_button_was_clicked(self):
-        self.button.setText("You already clicked me.")
-        self.button.setEnabled(False)
-        self.setWindowTitle("A new window title")
+        layout.addWidget(self.choose)
+        layout.addWidget(self.button)
         
+        widget = QWidget()
+        widget.setLayout(layout)
+            
+        self.setCentralWidget(widget)
 
-app = QApplication([])
+    def clicked(self):
+        x = self.choose.comboBox.currentText()
+        print('Current = ', x)
+        
+app = QApplication(sys.argv)
 
 window = MainWindow()
 window.show()
