925a74541fb394c53e8726ad8bcb2d97083dc037
ljp2 <ljp2@lehigh.edu>
2024-01-10 20:07:21 -0500

    working

--- backtestwindow.py
+++ backtestwindow.py
@@ -34,7 +34,7 @@ class BarPlotsFigure(QWidget):
         self.df = setup_df
         
         self.fig_layout = QVBoxLayout()
-        self.figure = Figure(figsize=(800,600))
+        self.figure = Figure(figsize=(1000,750))
         self.figure.suptitle("")
         self.figure.set_facecolor('#e6e6e6')
         self.canvas = FigureCanvasQTAgg(self.figure)
@@ -85,9 +85,9 @@ class BarPlotsFigure(QWidget):
         y = [self.y_low, self.y_high]
         # self.vline1 = Line2D(x,y, color='red', linestyle='--', label='Cursor')
         # self.ax1.add_line(self.vline1)
-        self.vline2 = Line2D(x,y, color='red', linestyle='--')
+        self.vline2 = Line2D(x,y, color='red', linestyle='--', linewidth=0.5)
         self.ax2.add_line(self.vline2)
-        self.vline3 = Line2D(x,y, color='red', linestyle='--')
+        self.vline3 = Line2D(x,y, color='red', linestyle='--', linewidth=0.5)
         self.ax3.add_line(self.vline3)
     
         self.canvas.mpl_connect('motion_notify_event', self.update_line)
@@ -213,12 +213,13 @@ class Buttons(QWidget):
 class BackTestWindow(QWidget):
     def __init__(self, base_file_name:str):
         super().__init__()
-        
+        self.value_per_point = 5
         self.current_price:float = None
         self.position:int = 0
-        self.dollars_invested = 0.0
         self.current_pl = 0.0
         self.day_pl = 0.0
+        
+        self.portfolio_share_value = 0.0
         
         self.df = utils.readBaseFile(basefilename=base_file_name)
         self.current_i = 0
@@ -259,14 +260,36 @@ class BackTestWindow(QWidget):
         print("REDRAW CLICKED")
         
     def handleBuy(self):
-        self.position += 1
+        if self.position == 0:
+            self.portfolio_share_value = self.current_price
+            self.position = 1
+        elif self.position < 0:
+            current_value_per_share = self.portfolio_share_value / (-self.position)
+            transaction_pl = current_value_per_share - self.current_price
+            self.day_pl += transaction_pl * self.value_per_point
+            self.position += 1
+            self.status.updateDayPL(round(self.day_pl,2))
+        else: # position > 0
+            self.portfolio_share_value += self.current_price
+            self.position += 1  
         self.status.updatePosition(self.position)
         
     def handleSell(self):
-        self.position -= 1
+        if self.position == 0:
+            self.portfolio_share_value = self.current_price
+            self.position = -1
+        elif self.position < 0:
+            self.portfolio_share_value += self.current_price
+            self.position -= 1
+        else: # position > 0
+            current_value_per_share = self.portfolio_share_value / self.position
+            transaction_pl = self.current_price -current_value_per_share
+            self.day_pl += transaction_pl * self.value_per_point
+            self.position -= 1
+            self.status.updateDayPL(round(self.day_pl,2))
         self.status.updatePosition(self.position)
         
-        
+
     def handleCreateHLine(self):
         self.horiz_line_create = not self.horiz_line_create
         if self.horiz_line_create:
